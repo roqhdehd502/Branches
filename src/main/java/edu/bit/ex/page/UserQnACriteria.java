b95c1d0f9d03c59bb799eb2914b0ec37ccb3858f
pyoLeo <p_yo_218@naver.com>
2021-03-29 18:19:29 +0900

    210329

---
+++ src/main/java/edu/bit/ex/page/UserQnACriteria.java
@@ -0,0 +1,22 @@
+package edu.bit.ex.page;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+public class UserQnACriteria {
+	private int pageNum; // 페이지 번호
+	private int amount; // 한 페이지당 몇 개의 데이터를 보여줄것인가?
+
+	public UserQnACriteria() {
+		this(1, 10); // 기본 값 1페이지를 10개로 지정한다
+	}
+
+	public UserQnACriteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+	}
+}
