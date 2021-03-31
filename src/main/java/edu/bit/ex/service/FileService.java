33175b535363371148953c7f5cd9c7d73721f39e
Minwoo Na <natorque5552@naver.com>
2021-03-31 21:59:18 +0900

    21.03.31 #4

--- src/main/java/edu/bit/ex/service/FileService.java
+++ src/main/java/edu/bit/ex/service/FileService.java
@@ -2,12 +2,7 @@ package edu.bit.ex.service;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import edu.bit.ex.vo.BoardVO;
-
 public interface FileService {
-	// 보드 ID 가져오기
-	public BoardVO getBoardID(int board_id);
-
 	// 파일 업로드
 	public void saveFile(MultipartFile file);
 }
