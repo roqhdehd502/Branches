f045f95b345d942e24b28215d90ce86393e030b0
jijiel <eunji6026@gmail.com>
2021-04-08 01:34:18 +0900

    210408. 장바구니 기능 구현중

--- src/main/java/edu/bit/ex/service/OrderServiceImpl.java
+++ src/main/java/edu/bit/ex/service/OrderServiceImpl.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import edu.bit.ex.mapper.OrderMapper;
-import edu.bit.ex.vo.CartVO;
+import edu.bit.ex.vo.PrdctVO;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -18,18 +18,16 @@ public class OrderServiceImpl implements OrderService {
 
 	private OrderMapper orderMapper;
 
-	// 상품 장바구니 담기 common
-	/*
-	 * @Override public void PrdDetailCartIn(PrdctVO prdctVO) { log.info("PrdDetailCartIn"); ejMapper.PrdDetailCartIn(prdctVO);
-	 * 
-	 * }
-	 */
-
-	// 장바구니 common
+	// 장바구니 리스트
 	@Override
-	public List<CartVO> getCartList() {
-		log.info("getCartList");
+	public List<PrdctVO> getCartList() {
+		log.info("getCartList()");
 		return orderMapper.getCartList();
+	}
+
+	@Override
+	public PrdctVO getProduct(String prdct_id) {
+		return orderMapper.getProduct(prdct_id);
 	}
 
 }
