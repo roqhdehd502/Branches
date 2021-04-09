65716699679bc44136cf61f0e2640089c74d52da
pyoLeo <p_yo_218@naver.com>
2021-04-09 18:17:16 +0900

    210409

--- src/main/java/edu/bit/ex/service/FileService.java
+++ src/main/java/edu/bit/ex/service/FileService.java
@@ -3,8 +3,7 @@ package edu.bit.ex.service;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileService {
+
 	// 파일 업로드
 	public void saveFile(MultipartFile file);
-
-	// PrdctImageVO getMagazineImage(int image_number);
 }
