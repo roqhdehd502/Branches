43b7378ac3018940279d78776b4ffefaf47262f4
Minwoo Na <natorque5552@naver.com>
2021-04-13 10:05:54 +0900

    #5

--- src/main/java/edu/bit/ex/vo/BoardVO.java
+++ src/main/java/edu/bit/ex/vo/BoardVO.java
@@ -2,15 +2,18 @@ package edu.bit.ex.vo;
 
 import java.sql.Date;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
+
 /*
 BOARD_ID	NUMBER
 BOARD_NAME	VARCHAR2(100 BYTE)
-BOARD_CONTENT	VARCHAR2(2000 BYTE)
+BOARD_CONTENT	VARCHAR2(3000 BYTE)
 MBR_ID	VARCHAR2(50 BYTE)
 BOARD_TYPE_NUMBER	NUMBER
 INQUIRY_NUMBER	NUMBER
@@ -20,7 +23,7 @@ ORDER_NUMBER	NUMBER
 BOARD_LIKE	NUMBER
 BOARD_HIT	NUMBER
 BOARD_STARRATE	NUMBER
-BOARD_CONTENT	VARCHAR2(1000 BYTE)
+BOARD_THUMBNAIL	VARCHAR2(1000 BYTE)
 */
 
 @AllArgsConstructor
@@ -42,4 +45,8 @@ public class BoardVO { // 게시판
 	private int board_hit; // 게시글조회수
 	private int board_starrate; // 게시글별점수
 	private String board_thumbnail; // 게시글썸네일
+
+	private MultipartFile[] uploadfiles; // 파일 업로드
+	private String onedeletefiles; // 이미지만 삭제
+	private String deletefiles; // 게시글과 이미지 삭제
 }
