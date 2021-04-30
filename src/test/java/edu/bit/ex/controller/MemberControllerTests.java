13ed93c41fc5f197b2208aff42e9050dd44bfd04
jijiel <eunji6026@gmail.com>
2021-04-30 18:11:31 +0900

    210430 테스트

--- src/test/java/edu/bit/ex/controller/MemberControllerTests.java
+++ src/test/java/edu/bit/ex/controller/MemberControllerTests.java
@@ -1,44 +1,64 @@
 package edu.bit.ex.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import edu.bit.ex.vo.PrdctVO;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@ExtendWith(SpringExtension.class)
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @AutoConfigureMockMvc
-public class MemberControllerTests { // 회원 컨트롤러 테스트
+public class MemberControllerTests {
+
+	@Autowired
+	MemberController memberController;
+
+	@Autowired
+	ObjectMapper mapper;
+
 	@Autowired
 	private MockMvc mockMvc;
 
 	@Test
 	@WithUserDetails("defg1234")
-	public void getMappingTest() throws Exception {
-		// 회원 마이페이지
+	public void member_mypageTest() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/member/mypage")).andExpect(MockMvcResultMatchers.status().isOk());
-		// 회원 정보 조회 페이지
-		mockMvc.perform(MockMvcRequestBuilders.get("/member/myinfo")).andExpect(MockMvcResultMatchers.status().isOk());
-		// 상품 Q&A 마이페이지
-		mockMvc.perform(MockMvcRequestBuilders.get("/member/mypage/prdctq/list")).andExpect(MockMvcResultMatchers.status().isOk());
-		// 유저 Q&A 마이페이지
-		mockMvc.perform(MockMvcRequestBuilders.get("/member/mypage/myqna/list")).andExpect(MockMvcResultMatchers.status().isOk());
-		// 찜하기 목록 페이지
-		mockMvc.perform(MockMvcRequestBuilders.get("/member/mypage/like")).andExpect(MockMvcResultMatchers.status().isOk());
-		// 최근 본 상품 페이지
-		mockMvc.perform(MockMvcRequestBuilders.get("/member/mypage/recently")).andExpect(MockMvcResultMatchers.status().isOk());
-		// 작성한 상품 리뷰 페이지
-		mockMvc.perform(MockMvcRequestBuilders.get("/member/mypage/review/list")).andExpect(MockMvcResultMatchers.status().isOk());
-		// 주문내역 조회 페이지
-		mockMvc.perform(MockMvcRequestBuilders.get("/member/mypage/order")).andExpect(MockMvcResultMatchers.status().isOk());
 	}
+
+	@Before // @Test 이전에 실행
+	public void setupForproductQnARegisterTest() {
+		this.mockMvc = MockMvcBuilders.standaloneSetup(memberController).build();
+		mapper = new ObjectMapper();
+	}
+
+	@Test
+	@WithUserDetails("defg1234")
+	public void adminQnA_commentTest() throws Exception {
+		PrdctVO prdct = new PrdctVO();
+		prdct.setPrdct_id("p08");
+
+		String content = mapper.writeValueAsString(prdct);
+
+		mockMvc.perform(post("/prdct/p08/qna/write") // postmapping test
+				.content(content).contentType(MediaType.APPLICATION_JSON)) //
+				.andReturn();
+	}
+
 }
